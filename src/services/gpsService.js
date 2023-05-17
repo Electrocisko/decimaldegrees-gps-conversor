c71cfd1dc3d900ce0d457578a7c0e1e7e70544d7
Francisco Zuidwijk <electrocisko@gmail.com>
2023-05-17 14:15:47 -0300

    Se agrego una condicional que cambia los numeros a decimasl con puntos si le llega con comas

--- src/services/gpsService.js
+++ src/services/gpsService.js
@@ -1,11 +1,19 @@
 import gpsCoordinates from "../helpers/gpsCoordinates.js";
 
 const gpsService = (dd) => {
-    let splitter = dd.split(",");
-    let lat = parseFloat(splitter[0]);
-    let long = parseFloat(splitter[1]);
-    const result = gpsCoordinates(lat, long);
 
+    let cantComas = dd.split(",").length;
+    let splitter = dd.split(" ");
+    let lat = splitter[0];
+    let long = splitter[1];
+
+    if (cantComas > 2) { // Si tiene mas de  dos comas significa que los numeros usan el sistema decimal con comas
+        lat = lat.replace(/,/g, '.'); 
+        long = long.replace(/,/g, '.');
+    } 
+    lat = parseFloat(lat);
+    long = parseFloat(long);
+    const result = gpsCoordinates(lat, long);
     return result
 }
 
